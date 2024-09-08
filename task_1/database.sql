f8a3504aab84bd7e4906d15cdec4f34bf407b2e5
Brianoyaro <brianoyaro2000@yahoo.com>
2024-09-08 22:17:10 +0300

    fixed mysql on local machine

--- task_1/database.sql
+++ task_1/database.sql
@@ -1,21 +1,20 @@
---create a database
+-- create a database
 CREATE DATABASE IF NOT EXISTS toys_db;
 
 USE toys_db;
 
 -- create a table
 CREATE TABLE IF NOT EXISTS toys(
-	id INT PRIMARY KEY AUTO INCREMENT NOT NULL,
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	name VARCHAR(80) UNIQUE NOT NULL,
 	description TEXT NOT NULL,
-	price DECIMAL DEFAULT 0.00,
-	origin_country VARCHAR(200) DEFAULT 'UK',
+	price DECIMAL(10, 2) DEFAULT 0.00,
+	origin_country VARCHAR(200) DEFAULT 'UK'
 );
 
 -- INSERT DUMMY DATA
 
-INSERT INTO toys (name, description, price, origin_country) VALUES (
+INSERT INTO toys (name, description, price, origin_country) VALUES
 	('ScoobyDoo', 'ScoobyDoo action toy from the infamous mysteryinc franchise', 12.00, 'US'),
 	('Thomas Train', 'Thomas the friendly engine', 4.97, 'KE'),
-	('Teddy', 'A Teddy bear whom we all love and adore', 17.20, 'US'),
-);
+	('Teddy', 'A Teddy bear whom we all love and adore', 17.20, 'US');
--- task_1/toys.js
+++ task_1/toys.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host     : 'localhost',
-  user     : 'root',
-  password : '',
+  user     : 'brian',
+  password : 'hossana001',
   database : 'toys_db'
 });
 
